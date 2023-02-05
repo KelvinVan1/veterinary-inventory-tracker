b9144a8605dfb752fd74879d93aac9b32c793c01
Kelvin Van <kelvinvan.cs@gmail.com>
2023-02-05 15:02:25 -0800

    Boilerplate for backend code. Adjustments for everything to run together   Boilerplate for express backend has been created and can be run using     npm run dev   Configuration changes made to configs to have everything run together

--- src/client/electron/index.cts
+++ src/client/electron/index.cts
@@ -1,12 +1,14 @@
 const {app, BrowserWindow} = require('electron');
+const path = require('path')
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1300,
-    height: 800
-  })
+    height: 800,
+  });
 
-  mainWindow.loadURL('http://localhost:5173'); //Connect to site (this will need npm run dev first)
+  mainWindow.loadURL('http://localhost:5173');
+  mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
---
+++ src/server/server.ts
@@ -0,0 +1,38 @@
+import express, { NextFunction, Request, Response } from 'express';
+import path from 'path';
+
+const app = express()
+const PORT = 5050;
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// serves index.html page
+app.get('/', (req: Request, res: Response) => {
+  return res.sendFile(path.resolve(__dirname, '../../index.html'))
+})
+
+// Catch for invalid request
+app.use('/*', (req: Request, res: Response) => {
+  return res.status(404).send("Invalid Page");
+});
+
+// Global error catching
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
+  const defaultErr = {
+    log: `GLOBAL ERROR HANDLER: caught unknown middleware error${err.toString()}`,
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  if (errorObj.log) console.log(errorObj);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+//Spin up backend on port
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
+
+export default app;
