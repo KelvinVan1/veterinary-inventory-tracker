8b077a3b2e44d37b9b52ac55355b0857261098a4
KelvinVan1 <90666201+KelvinVan1@users.noreply.github.com>
2023-07-25 00:12:13 -0700

    Core look for setup page completed functionality in progress

--- src/client/startup.tsx
+++ src/client/startup.tsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from 'react';
-import { redirect } from 'react-router-dom';
 
 const Startup = () => {
   const [loading, setLoading] = useState(true);
   const [configStatus, setConfigStatus] = useState(false);
   const [dbState, setDbState] = useState(0);
 
+  // Determines if the config file exists
   const checkConfig = async () => {
     try{
       if(configStatus === false){
         const request = await fetch('/api/config');
         const response = await request.json();
+        if(response.status === 1) {
+          setConfigStatus(true);
+        }
         setLoading(false);
-        if(response.status === 1) setConfigStatus(true);
       }
     } catch {
       await new Promise(resolve => setTimeout(resolve, 1000));
@@ -20,14 +22,14 @@ const Startup = () => {
     }
   };
 
+  // Checks to see if the database has been connected to
   const checkDB = async () => {
     const request = await fetch('/api/status');
     const response = await request.json();
     if(response.status === 0) {
       await fetch('/api/load');
     }
-      
-    setDbState(1);
+    else setDbState(1);
   };
 
   // Ensures that a config file does exist and if so we can change what we render
@@ -35,7 +37,7 @@ const Startup = () => {
     checkConfig();
   }, []);
 
-  // Checks on the status of the db and start up if not already
+  // Checks on the status of the db and start up if not already once config loads
   useEffect(() => {
     if(configStatus === true){
       checkDB();
@@ -57,8 +59,27 @@ const Startup = () => {
           <h1 className="text-white 2xl">Loading Please Wait...</h1>
         </div>
       }
+      
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen">
-        <h1 className="text-white 2xl">VetTrack Setup</h1>
+        <p className="flex items-center mb-6 text-2xl font-semibold text-white ">
+          VetTrack Setup
+        </p>
+
+        {/* Setup Section */}
+        <div className="font-bold w-full rounded-lg border max-w-md bg-gray-800 border-gray-700">
+          <div className="px-10 py-8 text-white space-y-4">
+            <h1 className="text-2xl">
+              Configuration settings
+            </h1>
+            <form onSubmit={() => console.log('placehold')} className="space-y-5">
+              <div>
+                <label htmlFor="mongoURI" className="block pb-3 text-md">Mongo URI</label>
+                <input type="text" id="mongoURI" placeholder="URI HERE" className="block border rounded-md w-full p-1.5 border-gray-700 bg-gray-600 text-gray-100"></input>
+              </div>
+              <button type="submit" className="w-full border border-blue-700 bg-blue-600 rounded-md p-1">Confirm</button>
+            </form>
+          </div>
+        </div>
       </div>
     </section>
   );
